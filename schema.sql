2617e6a6a80d241c0bd9a63678d73af0967f51c8
nickghughes <hughes.ni@husky.neu.edu>
2021-03-28 19:09:22 -0400

    categories

--- schema.sql
+++ schema.sql
@@ -20,14 +20,24 @@ CREATE TABLE `manufacturer`(
   UNIQUE INDEX `manufacturer_id_UNIQUE` (`id` ASC) VISIBLE
 );
 
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category`(
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `category_id_UNIQUE` (`id` ASC) VISIBLE
+);
+
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product`(
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` TEXT NOT NULL,
   `image_url` TEXT,
   `manufacturer_id` INT,
+  `category_id` INT,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturer`(`id`)
+  FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturer`(`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `category`(`id`)
   ON DELETE CASCADE ON UPDATE CASCADE,
   UNIQUE INDEX `product_id_UNIQUE` (`id` ASC) VISIBLE
 );
